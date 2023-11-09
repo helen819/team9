57f97b956f0defcbf0ee4866d56bdbaef416a2cd
helen819 <helenchoi@naver.com>
2023-11-09 22:32:26 +0900

    수정

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ with st.form("my_form1"):
     sid = st.text_input('Student ID:', autocomplete="on", placeholder="학번입력", max_chars=10)
     submitted = st.form_submit_button("조회")
     if submitted:
-        query = f"SELECT * FROM census.input_view WHERE age = {sid} limit 1"
-        st.write(common.run_bigquery(query))
+        query = f"SELECT * FROM student WHERE sid = '{sid}'"
+        st.write(common.postgres_select(query))
