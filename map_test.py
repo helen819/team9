0f869c6d1a3f9c99fd2d7ef0beeb8924527a1b9b
helen819 <helenchoi@naver.com>
2023-11-09 23:33:56 +0900

    map 2개

--- map_test.py
+++ map_test.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-query = f"SELECT * FROM station limit 100"
+query = f"SELECT * FROM station where stname = '서울대입구역'"
 response = common.postgres_select(query)
 
 # print(response)
@@ -12,11 +12,28 @@ response = common.postgres_select(query)
 df = pd.DataFrame({
     "col1": response['latitude'],
     "col2": response['longitude'],
+    "col3": 700,
+    "col4": '#0044ff',
+})
+
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
+
+
+query = f"SELECT * FROM station limit 100"
+response = common.postgres_select(query)
+
+df2 = pd.DataFrame({
+    "col1": response['latitude'],
+    "col2": response['longitude'],
     "col3": np.random.randn(100) * 100,
     "col4": np.random.rand(100, 4).tolist(),
 })
 
-st.map(df,
+st.map(df2,
     latitude='col1',
     longitude='col2',
     size='col3',
