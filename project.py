87ff5231a57552e167eafea4370d57e996427858
helen819 <helenchoi@naver.com>
2023-11-10 17:16:08 +0900

    매물 조회 화면

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ import common
 import streamlit as st
 import sqlite3
 import hashlib
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, ColumnsAutoSizeMode
+import pandas as pd
 
 def add_userdata(username, userid, password):
     common.postgres_update(f"INSERT INTO users(name, id, password) VALUES ('{username}', '{userid}', '{password}')")
@@ -17,10 +19,17 @@ def check_user(userid):
     response = common.postgres_select(f"SELECT * FROM users WHERE id = '{userid}'")
     return len(response)>0
 
+def get_buildings() :
+    return common.postgres_select(f"SELECT * FROM transactions t, building b WHERE t.building = b.loc and t.buyer IS NULL ORDER BY t.register_date desc limit 100")
+
+def get_buildings_with_address(address) :
+    return common.postgres_select(f"SELECT * FROM transactions t, building b WHERE t.building = b.loc and t.building like '%{address}%'")
+
+
 def main():
     st.title("건물공유중개 및 가격예측서비스")
 
-    menu = ["Home", "Login", "SignUp", "Add Building Info"]
+    menu = ["Home", "Login", "SignUp", "Add Building Info", "매물 조회"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if 'userid' not in st.session_state:
@@ -72,6 +81,62 @@ def main():
                 st.success("Building Information Added Successfully")
         else:
             st.warning("Please Login First to Add Building Information")
+
+    elif choice == "매물 조회" :
+
+        instr = ""
+        submitted = ""
+        with st.form('chat_input_form'):
+            st.markdown("##### 찾으려는 매물의 주소를 입력하세요.")
+            col1, col2 = st.columns([7,1]) 
+
+            with col1:
+                address = st.text_input(
+                    instr,
+                    value=instr,
+                    placeholder=instr,
+                    label_visibility='collapsed'
+                )
+
+            with col2:
+                submitted = st.form_submit_button('검색')
+        
+        df =""
+        if submitted:
+            df = get_buildings_with_address(address)
+            if len(df) == 0 : 
+                st.warning("해당 주소의 매물이 존재하지 않습니다.")
+        else :
+            df = get_buildings()
+        
+        gb = GridOptionsBuilder.from_dataframe(df[['building','expected_selling_price']])
+        gb.configure_selection(selection_mode="single")
+        gridOptions = gb.build()
+
+        data = AgGrid(df,
+            gridOptions=gridOptions,
+            enable_enterprise_modules=True,
+            allow_unsafe_jscode=True,
+            update_mode=GridUpdateMode.SELECTION_CHANGED,
+            columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW)
+        
+        selected_rows = data["selected_rows"]
+        
+        if len(selected_rows) != 0:
+            col1, col2, col3, col4 = st.columns(4)
+
+            with col1:
+                st.markdown("##### year")
+                st.markdown(f":orange[{selected_rows[0]['year']}]")
+            with col2:
+                st.markdown("##### price")
+                st.markdown(f":orange[{selected_rows[0]['price']}]")
+            with col3:
+                st.markdown("##### land_ratio")
+                st.markdown(f":orange[{selected_rows[0]['land_ratio']}]")
+            with col4:
+                st.markdown("##### floor_ratio")
+                st.markdown(f":orange[{selected_rows[0]['floor_ratio']}]")
 
 if __name__ == '__main__':
     main()
